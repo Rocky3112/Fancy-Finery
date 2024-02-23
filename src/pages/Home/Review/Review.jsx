6165d39ac0120e05f6cc416e8f417c2b4670cff3
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-02-23 12:33:11 +0600

    added review section and responsiveness done

--- src/pages/Home/Review/Review.jsx
+++ src/pages/Home/Review/Review.jsx
@@ -2,7 +2,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import review from '../../../../public/review.json'
-import Rating from "react-rating";
+
 import RatingStar from "./RatingStar";
 
 const Review = () => {
